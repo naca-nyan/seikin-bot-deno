b2825de8cd172ad6f8c7e2864367fb53bb375cb8
naca-nyan <naca.nyan@gmail.com>
2023-06-12 01:22:42 +0900

    :sparkles: Post json directly

--- main.ts
+++ main.ts
@@ -1,26 +1,12 @@
 import { serve } from "https://deno.land/std@0.155.0/http/server.ts";
-import discordwebhook from "https://deno.land/x/discordwebhook@v1.5.1/mod.ts";
 
 const WEBHOOK_URL = Deno.env.get("WEBHOOK_URL");
-
 if (!WEBHOOK_URL) {
   throw new Error("set WEBHOOK_URL env");
 }
-const webhook = new discordwebhook(WEBHOOK_URL);
-const decoder = new TextDecoder();
 
-const handler = async (req: Request) => {
-  if (req.method == "POST") {
-    await req.body?.pipeTo(
-      new WritableStream({
-        async write(chunk: Uint8Array) {
-          const content = decoder.decode(chunk);
-          const res = await webhook.createMessage(content);
-          console.log(res);
-        },
-      })
-    );
-  }
+const handler = (req: Request) => {
+  if (req.method == "POST") return fetch(WEBHOOK_URL, req);
   return new Response(Deno.readFileSync("./index.html"));
 };
 
