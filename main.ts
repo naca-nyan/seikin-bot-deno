3574195d2d539743a9c19ecdf4eda73f5df5dc36
naca-nyan <naca.nyan@gmail.com>
2023-06-15 00:42:31 +0900

    :sparkles: Add fileserver

--- main.ts
+++ main.ts
@@ -1,13 +1,16 @@
 import { serve } from "https://deno.land/std@0.155.0/http/server.ts";
+import { serveDir } from "https://deno.land/std@0.155.0/http/file_server.ts";
 
 const WEBHOOK_URL = Deno.env.get("WEBHOOK_URL");
 if (!WEBHOOK_URL) {
   throw new Error("set WEBHOOK_URL env");
 }
 
+const indexPage = Deno.readFileSync("index.html");
 const handler = (req: Request) => {
   if (req.method == "POST") return fetch(WEBHOOK_URL, req);
-  return new Response(Deno.readFileSync("./index.html"));
+  if (new URL(req.url).pathname === "/") return new Response(indexPage);
+  return serveDir(req);
 };
 
 serve(handler);
